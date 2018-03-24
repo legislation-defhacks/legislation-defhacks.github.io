ee7c13cfbd86787448de516efab9b2cdcd59d46d
Karishma Mandyam <karishma.mandyam@gmail.com>
2018-03-24 15:39:26 -0700

    accessing API through python works

--- classify.py
+++ classify.py
@@ -1,5 +1,7 @@
 import json
 import requests
 
-r = requests.get('https://api.propublica.org/congress/v1/members/A000360/votes.json')
-print(r)
+payload = {'X-API-Key': 'FDBDTKNO6cAmhbDXpAACrvLL82JRryke3RN7oU9'}
+r = requests.get('https://api.propublica.org/congress/v1/members/A000360/votes.json', headers=payload)
+# r.json()
+print(r.json())

5d6e5a1f07e3906816055073db0905288abf4d0b
nicolekriley <nriley16@uw.edu>
2018-03-25 09:16:43 -0700

    connecting

--- index.js
+++ index.js
@@ -8,14 +8,73 @@
 	/*Sets up event listeners for button pressing and sets up the puzzle pieces and the background.
 	selector for the page. */
 	window.onload = function() {
+		document.getElementById("submit").onclick = buildPopupDom;
 		//addStatesToDropDown(); 
 		//document.getElementById("searchbystate").onclick = findRepresentatives; 
 	};
+
+	// Given an array of URLs, build a DOM list of those URLs in the
+// browser action popup.
+function buildPopupDom() {
+  var radiosBill = document.getElementsByName('bill');
+  var valueBill = "";
+  for(int i = 0; i < radiosBill.length; i++) {
+    if(radiosBill[i].checked) {
+      valueBill = radiosBill[i].value;
+      break;
+    }
+  }
+  var radiosPerson = document.getElementsByName('person');
+  var valuePerson = "";
+  for(int i = 0; i < radiosPerson.length; i++) {
+    if(radiosPerson[i].checked) {
+      valuePerson = radiosPerson[i].value;
+      break;
+    }
+  }
+  var array = [valueBill, valuePerson];
+  var valueFirstRadio = document.getElementById("")
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:5000/spectrum",
+    data: {
+      urlArray: JSON.stringify(array)
+    },
+    success: function( result ) {
+      $( "#finalscore" ).html( "<strong>" + result + "%</strong>" );
+    }
+  });
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:5000/credibility",
+    data: {
+      urlArray: JSON.stringify(array)
+    },
+    success: function( result ) {
+        var creditiblity = "";
+        result = parseInt(result);
+        if(0 <= result && result <= 33){
+             creditiblity = "Low Credibility";
+        }else if(34 < result && result  <= 67){
+             creditiblity = "Medium Credibility";
+        }else if(68 < result && result <= 100){
+             creditiblity = "High Credibility";
+        }
+      $("#credibility_title").text(creditiblity);
+      $(".credibility_ticker").css("margin-left", result + "%");
+      $( "#credibility" ).html( "<strong>" + Math.abs(result) + "%</strong>" );
+      $("#suggest_credibility").text(creditiblity);
+    }
+  });
+}
+
 
 	//setting up our ajax request to get data. 
 	//takes in the url that we are requesting and the 
 	//onload function that is used once the data is loaded. 
 	function ajaxRequest(url, onloadFunction) {
+		document.getElementById("")
 		var ajax = new XMLHttpRequest(); 
 		ajax.onload = onloadFunction; 
 		ajax.open("GET", url, true);

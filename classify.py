6a7b49e3696e9e8613f4301aa64ef92466b85ed1
Karishma Mandyam <karishma.mandyam@gmail.com>
2018-03-24 17:08:42 -0700

    adding in sklearn

--- classify.py
+++ classify.py
@@ -1,13 +1,28 @@
 import json
 import requests
 
+import numpy as np
+from sklearn import linear_model, datasets
+
 payload = {'X-API-Key': 'FDBDTKNO6cAmhbDXpAACrvLL82JRryke3RN7oU9'}
 request = requests.get('https://api.propublica.org/congress/v1/members/K000388/votes.json', headers=payload)
 jsonParse = request.json()
 
 billsAndVotes = jsonParse["results"][0]["votes"]
 
-# memberData = []
+X = []
+Y = []
 for bill in billsAndVotes:
+    data = []
+    data.append(bill["total"]["yes"])
+    data.append(bill["total"]["no"])
+    # data.append(bill["total"][])
+    Y.append(bill["position"])
     print(bill["bill"]["bill_id"])
-# print()
+    # print(bill["position"])
+    X.append(data)
+
+logreg = linear_model.LogisticRegression(C=1e5)
+logreg.fit(X, Y)
+Z = []
+logreg.predict(Z)

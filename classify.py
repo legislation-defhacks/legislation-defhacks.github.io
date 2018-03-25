ce0cc87b4fd12fc7874c4d2f678d525be48e6475
Karishma Mandyam <karishma.mandyam@gmail.com>
2018-03-24 17:53:33 -0700

    added some data wrangling stuff

--- classify.py
+++ classify.py
@@ -1,28 +1,49 @@
 import json
 import requests
+import os
+
 
 import numpy as np
 from sklearn import linear_model, datasets
 
 payload = {'X-API-Key': 'FDBDTKNO6cAmhbDXpAACrvLL82JRryke3RN7oU9'}
-request = requests.get('https://api.propublica.org/congress/v1/members/K000388/votes.json', headers=payload)
-jsonParse = request.json()
+# request = requests.get('https://api.propublica.org/congress/v1/members/K000388/votes.json', headers=payload)
+# jsonParse = request.json()
 
-billsAndVotes = jsonParse["results"][0]["votes"]
+bills = []
+path = 'data/votes/2017/'
 
-X = []
-Y = []
-for bill in billsAndVotes:
-    data = []
-    data.append(bill["total"]["yes"])
-    data.append(bill["total"]["no"])
-    # data.append(bill["total"][])
-    Y.append(bill["position"])
-    print(bill["bill"]["bill_id"])
-    # print(bill["position"])
-    X.append(data)
+for folder in os.listdir(path):
+    filename = path + str(folder) + '/data.json'
+    json_data = json.load(open(filename))
+    if 'bill' in json_data:
+        if json_data["chamber"] == 'h':
+            bill = []
+            bill.append(json_data["bill"]["type"])
+            bill.append(json_data["category"])
+            bill.append(json_data["requires"])
+            bill.append(json_data["type"])
+            print(bill)
+            bills.append(bill)
 
-logreg = linear_model.LogisticRegression(C=1e5)
-logreg.fit(X, Y)
-Z = []
-logreg.predict(Z)
+
+
+
+
+
+# X = []
+# Y = []
+# for bill in billsAndVotes:
+#     data = []
+#     data.append(bill["total"]["yes"])
+#     data.append(bill["total"]["no"])
+#     # data.append(bill["total"][])
+#     Y.append(bill["position"])
+#     print(bill["bill"]["bill_id"])
+#     # print(bill["position"])
+#     X.append(data)
+#
+# logreg = linear_model.LogisticRegression(C=1e5)
+# logreg.fit(X, Y)
+# Z = []
+# logreg.predict(Z)
